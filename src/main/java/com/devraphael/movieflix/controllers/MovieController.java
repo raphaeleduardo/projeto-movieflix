9cf1df7175aa9cf9864e3223ff27c771e8012fc7
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-07-10 03:50:32 -0300

    GET Movie by id, MovieCOntrollerIT tests 1/2 OK

---
+++ src/main/java/com/devraphael/movieflix/controllers/MovieController.java
@@ -0,0 +1,29 @@
+package com.devraphael.movieflix.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devraphael.movieflix.dto.MovieDetailsDTO;
+import com.devraphael.movieflix.services.MovieService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieController {
+
+	@Autowired
+	private MovieService service;
+	
+	@PreAuthorize("hasAnyRole('ROLE_VISITOR', 'ROLE_MEMBER')")
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<MovieDetailsDTO>  findById(@PathVariable Long id) {
+		MovieDetailsDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+
+
+}
--- src/main/java/com/devraphael/movieflix/services/MovieService.java
+++ src/main/java/com/devraphael/movieflix/services/MovieService.java
@@ -1,8 +1,27 @@
 package com.devraphael.movieflix.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devraphael.movieflix.dto.MovieDetailsDTO;
+import com.devraphael.movieflix.entities.Movie;
+import com.devraphael.movieflix.repositories.MovieRepository;
+import com.devraphael.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class MovieService {
-	
+
+	@Autowired
+	private MovieRepository repository;
+
+	@Transactional(readOnly = true)
+	public MovieDetailsDTO findById(Long id) {
+		Optional<Movie> obj = repository.findById(id);
+		Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Movie not found"));
+		return new MovieDetailsDTO(entity, entity.getGenre());
+	}
+
 }
